6b41d0dd7fd6bf6dffa278b92dcb552de5dfa802
Wagner Souza <wagh.cwb@gmail.com>
2019-03-25 12:11:01 -0300

    Adds option to check an array of globals
    fixes #1

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-function getGlobal() { 
-  if (typeof globalThis !== 'undefined') { return globalThis; } 
-  if (typeof self !== 'undefined') { return self; } 
-  if (typeof window !== 'undefined') { return window; } 
-  if (typeof global !== 'undefined') { return global; } 
-  throw new Error('unable to locate global object'); 
-} 
+function getGlobal() {
+  if (typeof globalThis !== 'undefined') { return globalThis; }
+  if (typeof self !== 'undefined') { return self; }
+  if (typeof window !== 'undefined') { return window; }
+  if (typeof global !== 'undefined') { return global; }
+  throw new Error('unable to locate global object');
+}
 
-module.exports = function awaitGlobal(key, wait) {
+module.exports = function awaitGlobal(keys, wait = 300) {
   const root = getGlobal();
-  return new Promise((resolve, reject) => {
-    let timer;
-    const loop = () => {
-      if(root[key]) {
-        resolve(root[key]);
-        clearInterval(timer);
-      }
-    };
-    timer = setInterval(loop, wait || 300);
-    loop();
-  });
+  return Promise.all([].concat(keys).map(key => {
+    return new Promise(resolve => {
+      let timer;
+      const loop = () => {
+        if (root[key]) {
+          resolve(root[key]);
+          clearInterval(timer);
+        }
+      };
+      timer = setInterval(loop, wait);
+      loop();
+    });
+  }))
 };
